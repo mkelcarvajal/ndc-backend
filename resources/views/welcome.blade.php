301f55976162f67468404558eb4ef4aad4adeed5
Cristian Torres <cristian.torresm@redsalud.gov.cl>
2021-05-20 08:46:04 -0400

    Primer Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="row page-title">
+    <div class="col-sm-4 col-xl-6">
+        <h4 class="mb-1 mt-0">Horizontal</h4>
+    </div>
+</div>
+
+@endsection
