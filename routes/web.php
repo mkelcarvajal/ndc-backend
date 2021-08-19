03d85cf9fc5e9f2fbaef8a1b7291ddb48170c3f4
Cristian Torres <cristian.torresm@redsalud.gov.cl>
2021-08-19 12:50:51 -0400

    holiwi

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app');
-});
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
