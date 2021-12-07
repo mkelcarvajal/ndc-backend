497c64bc007360c409de3909c0d946bb5c435b44
Sebastian Gonzalez M <sebastian.gonzalez.p@redsalud.gov.cl>
2021-12-07 14:58:32 -0300

    ubicacion reporte

--- app/Http/Controllers/pruebasController.php
+++ app/Http/Controllers/pruebasController.php
@@ -2163,7 +2163,7 @@ class pruebasController extends Controller
         
             $pdf = $pdf->output();
             
-            $file_location = public_path()."\\reportes\\".$data->rut_r.".pdf";
+            $file_location = "public/reportes/".$data->rut_r.".pdf";
             file_put_contents($file_location,$pdf);
 
             return $data->rut_r;
