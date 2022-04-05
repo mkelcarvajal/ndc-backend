da6f3efd2fc9c05d590563e80234d7b8d3537e85
Sebastian Ignacio Gonzalez montoya <sebastian.gonzalez.p@redsalud.gov.cl>
2022-04-05 11:28:14 -0400

    informe_independencia

--- app/Http/Controllers/pruebasController.php
+++ app/Http/Controllers/pruebasController.php
@@ -3170,7 +3170,6 @@ class pruebasController extends Controller
                     $I++;
                 }
             }
-            dd($I);
             //B
             $mas_b=array();
             $menos_b=array();
@@ -3382,6 +3381,8 @@ class pruebasController extends Controller
             $informe_independencia = DB::table('datos_informe')->selectRaw('descripcion,puntaje')->where('caracteristica','Independencia')->where('puntaje',polarizado($resultado_i->resultado))->first();
             $informe_variedad = DB::table('datos_informe')->selectRaw('descripcion,puntaje')->where('caracteristica','Variedad')->where('puntaje',polarizado($resultado_v->resultado))->first();
             $informe_benevolencia = DB::table('datos_informe')->selectRaw('descripcion,puntaje')->where('caracteristica','Benevolencia')->where('puntaje',polarizado($resultado_b->resultado))->first();
+
+            dd($informe_independencia);
 
             $informe_cautela = DB::table('datos_informe')->selectRaw('descripcion,puntaje')->where('caracteristica','Cautela')->where('puntaje',polarizado($resultado_cau->resultado))->first();
             $informe_originalidad = DB::table('datos_informe')->selectRaw('descripcion,puntaje')->where('caracteristica','Originalidad')->where('puntaje',polarizado($resultado_ori->resultado))->first();
