2e82e176d8323144899dba14c91420156bdebd49
Sebastian Ignacio Gonzalez montoya <sebastian.gonzalez.p@redsalud.gov.cl>
2022-05-18 11:51:30 -0400

    mysql_ndc

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_ndc' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_NDC', 'forge'),
+            'port' => env('DB_PORT_NDC', 'forge'),
+            'database' => env('DB_DATABASE_NDC', 'forge'),
+            'username' => env('DB_USERNAME_NDC', 'forge'),
+            'password' => env('DB_PASSWORD_NDC', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
