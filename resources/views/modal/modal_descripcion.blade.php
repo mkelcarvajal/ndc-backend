da50638d928fa3ac93722a770e8ce21890fb676e
Sebastian Ignacio Gonzalez montoya <sebastian.gonzalez.p@redsalud.gov.cl>
2022-07-20 09:41:29 -0400

    boton y lavel

--- resources/views/modal/modal_descripcion.blade.php
+++ resources/views/modal/modal_descripcion.blade.php
@@ -12,40 +12,44 @@
                 </div>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <form method="post" action="{{$data[0]->rut}}/pdf_diploma">
-                        @csrf
-                        <div class="row">
-                            <div class="col">
-                                <input type="hidden" class="form-control" id="id" name="id">
-                                <label for="nota_promedio" class=" mt-2">Nota:</label>
-                                <input type="text" readonly class="form-control mt-2" id="nota_promedio">
-                                <label for="nota_promedio" class=" mt-2">Fecha Inicio:</label>
-                                <input type="text" readonly class="form-control mt-2" id="fecha_inicio">
+                <form method="post" action="{{$data[0]->rut}}/pdf_diploma">
+                    <div class="modal-body">
+                            @csrf
+                            @if($data[0]->tipo_empresa == 'CODELCO')
+                                <center><img src="../img/codelco.jpg" width="100"  style="border-radius:7px;"></center>
+                            @endif
+                            <div class="row">
+                                <div class="col">
+                                    <input type="hidden" class="form-control" id="id" name="id">
+                                    <label for="nota_promedio" class=" mt-2">Nota:</label>
+                                    <input type="text" readonly class="form-control mt-2" id="nota_promedio">
+                                    <label for="nota_promedio" class=" mt-2">Fecha Inicio:</label>
+                                    <input type="text" readonly class="form-control mt-2" id="fecha_inicio">
+                                </div>
+                                <div class="col">
+                                    <label for="nota_promedio" class=" mt-2">Asistencia :</label>
+                                    <input type="text" readonly class="form-control mt-2" id="asistencia_promedio">
+                                    <label for="nota_promedio" class=" mt-2">Fecha Termino:</label>
+                                    <input type="text" readonly class="form-control mt-2" id="fecha_termino">
+                                </div>
                             </div>
-                            <div class="col">
-                                <label for="nota_promedio" class=" mt-2">Asistencia :</label>
-                                <input type="text" readonly class="form-control mt-2" id="asistencia_promedio">
-                                <label for="nota_promedio" class=" mt-2">Fecha Termino:</label>
-                                <input type="text" readonly class="form-control mt-2" id="fecha_termino">
+
+                            <div class="row">
+                                <div class="col text-center">
+                                    <label for="nota_promedio" class=" mt-2">Fecha Vigencia:</label>
+                                    <input type="text" readonly class="form-control mt-2 text-center" id="fecha_vigencia">
+                                </div>
                             </div>
-                        </div>
-                        
-                        <div class="row">
-                            <div class="col">
-                                <label for="nota_promedio" class=" mt-2">Fecha Vigencia:</label>
-                                <input type="text" readonly class="form-control mt-2 text-center" id="fecha_vigencia">
+                            <br>
+                            <div class="d-grid gap-2">
                             </div>
-                        </div>
-                        <br>
-                        <div class="d-grid gap-2">
-                            <button type="submit" class="btn btn-block btn-success"><i class="fa-solid fa-file-arrow-down"></i> Descargar Diploma  </button>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
-                </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-block btn-success"><i class="fa-solid fa-file-arrow-down"></i> Descargar Diploma  </button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
