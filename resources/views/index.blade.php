5563aa7dc7e91d6d383e503629d83b56f17ccae5
sgonzalez <iisebaii13@gmail.com>
2022-08-16 16:58:43 -0400

    ssda

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -162,7 +162,10 @@ function diferencia($fecha1,$fecha2){
                     $("#cargo").val(data.cargo);
                     $("#gerencia").val(data.gerencia);
 
-                    if(moment(data.vigencia).format("YYYY/MM/DD HH:MM:SS") > moment().format("YYYY/MM/DD HH:MM:SS")){
+                    var hoy = moment(moment(), 'YYYY-MM-DD HH:mm').toDate();
+                    var vigencia = moment(data.vigencia , 'YYYY-MM-DD HH:mm').toDate();
+
+                    if(vigencia > hoy){
                         $("#pie").append('<button type="submit"  class="btn btn-block btn-success"><i class="fa-solid fa-file-arrow-down"></i> Descargar Diploma  </button>\
                                           <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>')
                     }
