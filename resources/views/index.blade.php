d51591b3ade4cdc448c022afab1934b91a609fec
sgonzalez <iisebaii13@gmail.com>
2022-08-16 18:23:26 -0400

    valido string

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -162,7 +162,9 @@ function diferencia($fecha1,$fecha2){
                     $("#cargo").val(data.cargo);
                     $("#gerencia").val(data.gerencia);                    
 
-                    if(data.validacion == 'valido'){
+                    alert(data.validacion);
+
+                    if(data.validacion === 'valido'){
                         $("#pie").append('<button type="submit"  class="btn btn-block btn-success"><i class="fa-solid fa-file-arrow-down"></i> Descargar Diploma  </button>\
                                           <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>')
                     }
