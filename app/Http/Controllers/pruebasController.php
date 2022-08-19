f8385580ac327c642f458aeaa02af3e0ce798a25
sgonzalez <iisebaii13@gmail.com>
2022-08-19 15:09:31 -0400

    sda

--- app/Http/Controllers/pruebasController.php
+++ app/Http/Controllers/pruebasController.php
@@ -4015,7 +4015,6 @@ class pruebasController extends Controller
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename="BD_Socia.xlsx"');
         $writer->save('php://output');
-        
     }
 
     // public function indexPrueba()
